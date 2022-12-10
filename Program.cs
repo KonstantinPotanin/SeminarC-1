cd0dfc41e184d8d4eda7540e222d713864e455cc
Konstantin <pkv90@yandex.ru>
2022-12-10 23:11:11 +1200

    1 семинар знакомства с С#

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Задача 1
+/*
+Console.Write("Input a first number: ");
+int  num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int  num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if (num1 == quad)
+
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+Задача 2
+/*
+Console.Write("Input an integer number: ");
+int  num = Convert.ToInt32(Console.ReadLine());
+
+int corrent = -num;
+
+while(corrent <= num)
+{
+    Console.WriteLine(corrent + " ");
+    corrent++;
+    }
+    */
+
+
+Задача 3
+
+12345 / 10 = 1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1
+
+12345 % 10 = 5
+12345 % 100 = 45
+12345 % 1000 = 345
+12345 % 10000 = 2345
+12345 % 100000 = 12345
